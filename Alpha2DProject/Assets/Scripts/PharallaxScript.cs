eaa219a81360e93506f9f586602fc34e47f46944
ismaelbfh <ismaelcentella@gmail.com>
2017-07-30 14:26:05 +0100

    Commit Inicial con proyecto en nuestro repo
    - Proyecto Unity
    - Game Document Design

---
+++ Alpha2DProject/Assets/Scripts/PharallaxScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PharallaxScript : MonoBehaviour {
+
+    PlayerAndHUDController ControladorHud; //Referencia al script que controla los movimientos del usuario y los objetos de interfaz en pantalla.
+
+    private float offset; //Para cambiar el Offset
+
+    public float speed; //Velocidad que se le aplica
+
+    public float contador;
+
+    private void Start()
+    {
+        ControladorHud = GameObject.Find("HUD").GetComponent<PlayerAndHUDController>(); //coje el objeto entero de HUD con todos sus hijos
+    }
+
+    private void Update ()
+    {
+       if(ControladorHud.signoPharallax != 0){
+           ActivarPharallax(ControladorHud.signoPharallax);
+       }
+    }
+
+    private void ActivarPharallax(float signo)
+    {
+        offset = speed * contador * signo;
+        contador += Time.deltaTime;
+        GetComponent<Renderer>().material.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+    }
+}
