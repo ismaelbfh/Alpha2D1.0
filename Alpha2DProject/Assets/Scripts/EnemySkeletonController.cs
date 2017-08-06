0278b792172d52f25b659bdcbdacfd5f42006a46
Sproilex <30391055+Sproilex@users.noreply.github.com>
2017-08-06 10:39:21 -0430

    Creaciòn IA bàsica Enemigo.
    Se movio la capa llamada "Capa2" para ajustarlas exactamente a la posicion de sus predecesoras, pues tenia un pequeño borde de diferencia por unos pixeles a la izquierda.
    ________________________________
    
    Se creo un animatorController del enemigo esqueleto con sus 3 animaciones basicas, además de sus valores. Se utilizaron los mismos nombres de los valores del player para generalizar y se añadio un bool llamado "attack".
    
    ________________________________
    
    Se creo en la escena al enemigo, con la animacion de caminar como default. Además se añadio un boxCollider al esqueleto marcado como trigger que abarca la plataforma, servirá para reaccionar cuando el jugador suba a ella.
    
    ________________________________
    
    Se creo un script llamado "EnemySkeletonController" que manejará a cada esqueleto. El esqueleto camina y se da vuelta al final de la plataforma. Si el jugador se acerca a la plataforma el esqueleto lanza la animacion de ataque, cuando el jugador sale de la plataforma sigue caminando. Y si se marca la opción de salto realiza el impulso con su animación.
    ________________________________
    
    Se creo un script llamada "TriggersPlatforms" que será el encargado de avisar si el jugador a entrado en la plataforma y además avisa si el enemigo debe dar vuelta o no.

---
+++ Alpha2DProject/Assets/Scripts/EnemySkeletonController.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySkeletonController : MonoBehaviour {
+
+    //Variables del Enemigo:
+    GameObject Enemigo;
+    public float velocidadCaminar; 
+    public float fuerzaSalto;
+    public float vida = 1.0f;
+    public Rigidbody2D cuerpoEnemigo;
+    public Animator animatorEnemigo;
+    SpriteRenderer spriteRendererEnemigo;
+    public bool voltear;
+    public bool Saltar;
+    float contadorSalto;
+
+    //IA:
+    public bool JugadorEnPlataforma = false;
+
+    private void Start()
+    {
+        //Inicialización:
+        Enemigo = this.gameObject;
+        cuerpoEnemigo = Enemigo.GetComponent<Rigidbody2D>();
+        animatorEnemigo = Enemigo.GetComponent<Animator>();
+        spriteRendererEnemigo = Enemigo.GetComponent<SpriteRenderer>();
+    }
+
+
+    void FixedUpdate()
+    {
+        //Caminar y Atacar:
+        if (!JugadorEnPlataforma) // Si el jugador no esta en la plataforma no atacara y caminara:
+        {
+            cuerpoEnemigo.velocity = new Vector2(velocidadCaminar, cuerpoEnemigo.velocity.y);
+            DejarAtacar();
+        }
+        else //En cambio si el jugador esta en la plataforma no caminara y atacara:
+        {
+            Atacar();
+        }
+
+        //Voltearse al llegar al final de la plataforma:
+        if (voltear)
+        {
+            if (spriteRendererEnemigo.flipX == true)
+            {
+                spriteRendererEnemigo.flipX = false;
+                velocidadCaminar = -velocidadCaminar;
+                voltear = false;
+            }
+            else
+            {
+                spriteRendererEnemigo.flipX = true;
+                velocidadCaminar = Mathf.Abs(velocidadCaminar);
+                voltear = false;
+            }
+        }
+
+        if (Saltar && contadorSalto <= 0)
+        {
+            cuerpoEnemigo.velocity = new Vector3(0 /*Para que no se mueva mientras salta */, fuerzaSalto);
+            animatorEnemigo.SetFloat("vspeed", Mathf.Abs(cuerpoEnemigo.velocity.y));
+            Saltar = false;
+            contadorSalto = 3.0f;
+        }
+
+        if(contadorSalto <= 3.0f && contadorSalto > 0){
+            contadorSalto -= Time.deltaTime;
+            cuerpoEnemigo.velocity = new Vector3(cuerpoEnemigo.velocity.x, cuerpoEnemigo.velocity.y - Time.deltaTime);
+            animatorEnemigo.SetFloat("vspeed", Mathf.Abs(cuerpoEnemigo.velocity.y));
+            if(cuerpoEnemigo.velocity.y < 0){
+                animatorEnemigo.SetFloat("vspeed", 0);
+            }
+        }
+        else if (contadorSalto <= 0)
+        {
+            contadorSalto = 0;
+        }
+
+    }
+
+    private void Atacar()
+    {
+        animatorEnemigo.SetBool("attack",true);
+        //Crear el sprite del cuchillo en el segundo
+    }
+
+    private void DejarAtacar()
+    {
+        animatorEnemigo.SetBool("attack", false);
+    }
+
+}
---
+++ Alpha2DProject/Assets/Scripts/TriggersPlatforms.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggersPlatforms : MonoBehaviour {
+
+    public GameObject EsqueletoAsociado;
+    EnemySkeletonController controladorEsqueleto;
+    public bool EsEnemigo;
+
+    private void Start()
+    {
+       controladorEsqueleto = EsqueletoAsociado.GetComponent<EnemySkeletonController>();
+    }
+
+    void OnTriggerEnter2D(Collider2D Colision)
+    {
+        if (!EsEnemigo)
+        {
+            if (Colision.tag == "Player")
+            {
+                controladorEsqueleto.JugadorEnPlataforma = true;
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D Colision)
+    {
+        if (!EsEnemigo)
+        {
+            if (Colision.tag == "Player")
+            {
+                controladorEsqueleto.JugadorEnPlataforma = false;
+            }
+
+        }else if(EsEnemigo){
+
+        if (Colision.name == EsqueletoAsociado.name)
+        {
+            controladorEsqueleto.voltear = true;
+        }
+
+        }
+    }
+
+}
